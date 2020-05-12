aa5afb0366ae6ac812e20a426043ded517d600df
Katerina <katerina.raylyan@gmail.com>
2020-05-12 09:44:41 +0300

    fix: clean up linter errors

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -206,8 +206,8 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
-  
+function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
+  throw new Error('Not implemented');
 }
 
 
